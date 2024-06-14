ae6815e3e5d337ab2cce1e414a489af9e03b7483
Gautham <gauthamsritharan@gmail.com>
2024-06-14 21:18:41 +0200

    modification-errorfix

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ import spacy
 from textblob import TextBlob
 import ast
 
-# Load the SpaCy model
-nlp = spacy.load("en_core_web_sm")
+# Ensure spaCy model is downloaded
+def download_spacy_model():
+    try:
+        nlp = spacy.load("en_core_web_sm")
+    except OSError:
+        subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+        nlp = spacy.load("en_core_web_sm")
+    return nlp
 
+nlp = download_spacy_model()
 # Load datasets
 @st.cache_data
 def load_data():
